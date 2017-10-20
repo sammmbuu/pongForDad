447d3d8f3e48a5db5bd580fc375b5137085667eb
Sam Hearn <samhearn2010@gmail.com>
2017-10-20 16:03:04 +0000

    added ball object and functions

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var player1Score = 0;
 var player2Score = 0;
 
 //format canvas to look like pong
-cntx.beginPath();
-cntx.strokeStyle = "white";
-cntx.lineWidth = "10";
-cntx.setLineDash([20,15]);
-cntx.moveTo(canvas.width/2, 10);
-cntx.lineTo(canvas.width/2, canvas.height - 10);
-cntx.stroke();
+    cntx.beginPath();
+    cntx.strokeStyle = "white";
+    cntx.lineWidth = "10";
+    cntx.setLineDash([20,15]);
+    cntx.moveTo(canvas.width/2, 10);
+    cntx.lineTo(canvas.width/2, canvas.height - 10);
+    cntx.stroke();
 
 
 //paddle objects
@@ -18,4 +18,31 @@ var Paddle = function(x, y){
     this.x = x;
     this.y = y;
     this.speed = 2;
+    this.width = 15;
+    this.height = 40;
+};
+
+Paddle.prototype.create = function(){
+    cntx.fillStyle = "white";
+    cntx.fillRect(this.x, this.y, this.width, this.height);
+};
+
+Paddle.prototype.move = function(){
+    if(this.x < canvas.width){
+        canvas.addEventListener("onkeydown", function(e){
+            if (e.keycode === 87){
+                this.y = this.y - this.speed;
+            }   else if (e.keyCode === 83) {
+                this.y = this.y + this.speed;
+            }
+        });
+    }   else if(this.x > canvas.width){
+        canvas.addEventListener("onkeydown", function(e){
+            if (e.keycode === 38){
+                this.y = this.y - this.speed;
+            }   else if (e.keyCode === 40) {
+                this.y = this.y + this.speed;
+            }
+        });
+    }
 };
